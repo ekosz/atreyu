6043a8321dc64930bb789a9ab9e6a1ba774795a9
Eric Koslow <ekoslow@gmail.com>
2016-06-12 10:02:11 -0700

    Simplify footer

--- example/src/footer.jsx
+++ example/src/footer.jsx
@@ -1,44 +1,32 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-
-function selectedClass(left, right) {
-  return left === right ? 'selected' : '';
-}
+const filterProps = (type, filter, changeFilter) => ({
+  className: filter === type ? 'selected' : '',
+  onClick: () => changeFilter(type),
+});
 
 const Footer = ({ filter, changeFilter, count }) =>
   <footer className="footer">
     <span className="todo-count"><strong>{count}</strong> item left</span>
     <ul className="filters">
       <li>
-        <a
-          className={selectedClass('all', filter)}
-          data-name="all"
-          onClick={() => changeFilter('all')}
-        >
-          All
-        </a>
+        <a {...filterProps('all', filter, changeFilter)}>All</a>
       </li>
       <li>
-        <a
-          className={selectedClass('active', filter)}
-          data-name="active"
-          onClick={() => changeFilter('active')}
-        >
-          Active
-        </a>
+        <a {...filterProps('active', filter, changeFilter)}>Active</a>
       </li>
       <li>
-        <a
-          className={selectedClass('completed', filter)}
-          data-name="completed"
-          onClick={() => changeFilter('completed')}
-        >
-          Completed
-        </a>
+        <a {...filterProps('completed', filter, changeFilter)}>Completed</a>
       </li>
     </ul>
 
     <button className="clear-completed">Clear completed</button>
   </footer>;
+
+Footer.propTypes = {
+  filter: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+};
 
 export default Footer;
